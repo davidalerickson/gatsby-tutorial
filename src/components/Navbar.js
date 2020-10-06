b556abc643a37a08ef5450c6fc335f60e33acf38
David Erickson <daviderickson@davids-mbp.lan>
2020-10-06 13:48:55 -0400

    added styling to navbar componen t

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,26 @@ import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blog/">Blog</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-        <li>
-          <Link to="/examples">Examples</Link>
-        </li>
-        <li>
-          <Link to="/images">Images</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <ul className="nave-header">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog/">Blog</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/examples">Examples</Link>
+          </li>
+          <li>
+            <Link to="/images">Images</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
